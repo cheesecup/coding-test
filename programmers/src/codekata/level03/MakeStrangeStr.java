88734e7120aa5098d9624b3f6f8ee3608803bb5c
cheesecup <kdy8252@naver.com>
2024-02-26 12:08:37 +0900

    4일차 level3 문제 풀이 추가

---
+++ programmers/src/codekata/level03/MakeStrangeStr.java
@@ -0,0 +1,33 @@
+package codekata.level03;
+
+import java.util.Arrays;
+
+/* 이상한 문자 만들기 */
+class MakeStrangeStr {
+    public String solution(String s) {
+        String answer = "";
+        s = s.toLowerCase();
+        char[] arr = s.toCharArray();
+
+        int cnt = 0;
+        for (int i=0; i<arr.length; i++) {
+            if (arr[i] == ' ') {
+                cnt = 0;
+                continue;
+            }
+
+            if (cnt % 2 == 0) {
+                arr[i] -= 32;
+            }
+            cnt++;
+        }
+        answer = new String(arr);
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        MakeStrangeStr makeStrangeStr = new MakeStrangeStr();
+        System.out.println(makeStrangeStr.solution("try hello world"));
+    }
+}
